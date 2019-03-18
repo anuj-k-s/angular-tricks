0845728310904f7bb4563d7fcc87dea43df8de64
anuj-k-s <anujkumarsoni1993@gmail.com>
2019-03-18 21:36:00 +0530

    inline html & template file

---
+++ src/app/inline-html/inline-html.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inline-html',
+  templateUrl: './inline-html.component.html',
+  styleUrls: ['./inline-html.component.scss']
+})
+export class InlineHtmlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
