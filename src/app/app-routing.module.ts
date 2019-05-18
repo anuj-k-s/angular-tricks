e0d1805355369a4cca4941b48e9a2bf48661d7c3
anuj-k-s <anujkumarsoni1993@gmail.com>
2019-05-18 14:04:20 +0530

    changes for navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InlineHtmlComponent } from './inline-html/inline-html.component';
 import { TemplateBasicComponent } from './template-basic/template-basic.component';
+import { Java8Component } from './java8/java8.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 const routes: Routes = [
   {path : 'inline-html' , component : InlineHtmlComponent},
-  {path : 'basic-template' , component : TemplateBasicComponent}
+  {path : 'basic-template' , component : TemplateBasicComponent},
+  {path : 'java8' , component : Java8Component},
+  {path : 'navbar' , component : NavigationBarComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { InlineHtmlComponent } from './inline-html/inline-html.component';
 import { TemplateBasicComponent } from './template-basic/template-basic.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Java8Component } from './java8/java8.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InlineHtmlComponent,
     TemplateBasicComponent,
-    Java8Component
+    Java8Component,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
