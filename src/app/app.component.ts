fcbb624d7abce4dbdc4ec530035b3cf9cc6b021a
anuj kumar soni <anujkumarsoni1993@gmail.com>
2019-05-28 09:44:04 +0530

    changes for html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tips';
+  public flag = false;
+  constructor() {
+    setTimeout(() => {
+      this.flag = true;
+    }, 2000);
+  }
 }
