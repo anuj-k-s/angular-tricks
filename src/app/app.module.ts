7d49bb32b8fa9a099ef19dc6f7919d78d86f6a52
anuj-k-s <anujkumarsoni1993@gmail.com>
2019-06-30 17:38:34 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InlineHtmlComponent } from './inline-html/inline-html.component';
 import { TemplateBasicComponent } from './template-basic/template-basic.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Java8Component } from './java8/java8.component';
@@ -15,7 +14,6 @@ import { DataTableComponent } from './data-table/data-table.component';
 @NgModule({
   declarations: [
     AppComponent,
-    InlineHtmlComponent,
     TemplateBasicComponent,
     Java8Component,
     NavigationBarComponent,
